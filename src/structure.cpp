fb2a8b86c71daf30bac70b006f7d39231eafebb9
Grall Arnaud <folkvir@users.noreply.github.com>
2016-01-21 01:21:32 +0100

    Update structure.cpp

--- src/structure.cpp
+++ src/structure.cpp
@@ -1,48 +1,29 @@
 #include "structure.hpp"
 #include <iostream>
 
-/*
- * Génère un noeud de type Conc
- */
-Conc* GenConc(Node* pleft, Node* pright) {
-	Conc * pconc = new Conc;
-	pconc->left = pleft;
-	pconc->right = pright;
-	return pconc;
+Conc* GConc(Node* left, Node* right) {
+	Conc * conc = new Conc;
+	conc->left = left;
+	conc->right = right;
+	return conc;
 }
-
-/*
- * Génère un noeud de type Union
- */
-Union* GenUnion(Node* pleft, Node* pright) {
-	Union * punion = new Union;
-	punion->left = pleft;
-	punion->right = pright;
+Union* GUnion(Node* left, Node* right) {
+	Union * p_union = new Union;
+	p_union->left = left;
+	p_union->right = right;
 	return punion;
 }
-
-/*
- * Génère un noeud de type Star
- */
-Star * GenStar(Node* ptr) {
+Star * GStar(Node* ptr) {
 	Star * pstar = new Star;
-	pstar->Star_e = ptr;
+	p_star->star = ptr;
 	return pstar;
 }
-
-/*
- * Génère un noeud de type UN
- */
-Un * GenUN(Node* ptr) {
+Un * GUN(Node* ptr) {
 	Un * pun = new Un;
-	pun->Un_e = ptr;
+	pun->un = ptr;
 	return pun;
 }
-
-/*
- * Génère un noeud de type Atom
- */
-Atom* GenAtom(int code, int action, ATOMTYPE type) {
+Atom* GAtom(int code, int action, ATOMETYPES type) {
 	Atom * patom = new Atom;
 	patom->code = code;
 	patom->action = action;
